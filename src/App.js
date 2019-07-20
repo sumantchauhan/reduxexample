2d7623d1614295d68ee7f114f0a6c99ec7af0c29
sumantchauhan <sumant@chainflux.com>
2019-07-20 21:55:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "react-redux";
+import Posts from "./components/Posts";
+import PostForm from "./components/Postform";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="posts">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
